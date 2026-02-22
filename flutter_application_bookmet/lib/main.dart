0818028cbaf09610cbba672003d4189d4e8d0230
lbpm <luis.pena@correo.unimet.edu.ve>
2026-02-21 20:59:23 -0400

    Conexión con Supabase

--- flutter_application_bookmet/lib/main.dart
+++ flutter_application_bookmet/lib/main.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'firebase_options.dart';
 import 'screens/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  // Inicializamos Firebase
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // Inicializamos Supabase
+  await Supabase.initialize(
+    url: 'https://yreemoaspqrvggijwkcz.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlyZWVtb2FzcHFydmdnaWp3a2N6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3MTY5MzYsImV4cCI6MjA4NzI5MjkzNn0.zVmHoatjPkKLgkEb1Fj4eHYFBVu8thbqwncv6kw3qMQ',
+  );
+
   runApp(const MyApp());
 }
 
